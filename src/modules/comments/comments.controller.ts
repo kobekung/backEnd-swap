5ab12e2b9318caa59cd7d273b5b868f88f17db7b
OverBig <kobe1234.aaa@gmail.com>
2024-09-09 13:31:08 +0700

    comment

--- src/modules/comments/comments.controller.ts
+++ src/modules/comments/comments.controller.ts
@@ -1,14 +1,23 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Post, Body, Get, Query } from '@nestjs/common';
 import { CommentsService } from './comments.service';
-import { Comment } from './comment.entity';
+import { ICommented } from 'src/interface/Comments.interface';
 
 @Controller('comments')
 export class CommentsController {
   constructor(private readonly commentsService: CommentsService) {}
 
-  @Get(':id')
-  async getComment(@Param('id') id: number): Promise<Comment> {
-    return this.commentsService.findOne(id);
+  @Post('create')
+  async createComment(@Body() createCommentDto: ICommented) {
+    return this.commentsService.createComment(createCommentDto);
   }
 
+  @Get('product')
+  async getCommentsByProduct(@Query('product_id') product_id: number) {
+    return this.commentsService.getCommentsByProduct(product_id);
+  }
+
+  @Get('user')
+  async getCommentsByUser(@Query('user_id') user_id: number) {
+    return this.commentsService.getCommentsByUser(user_id);
+  }
 }
--- src/modules/comments/comments.service.ts
+++ src/modules/comments/comments.service.ts
@@ -2,18 +2,59 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Comment } from './comment.entity';
+import { User } from '../users/users.entity';
+import { Product } from '../products/products.entity';
+import { ICommented } from 'src/interface/Comments.interface';
 
 @Injectable()
 export class CommentsService {
   constructor(
     @InjectRepository(Comment)
-    private commentsRepository: Repository<Comment>,
+    private readonly commentRepository: Repository<Comment>,
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+    @InjectRepository(Product)
+    private readonly productRepository: Repository<Product>,
   ) {}
 
-  // Example method to find a comment by ID
-  async findOne(id: number): Promise<Comment | undefined> {
-    return this.commentsRepository.findOne({ where: { id } });
+  async createComment(createCommentDto: ICommented): Promise<Comment> {
+    const { user_id, product_id, content } = createCommentDto;
+  
+    // แก้การค้นหา user
+    const user = await this.userRepository.findOneBy({ id: user_id });
+    if (!user) {
+      throw new Error('User not found');
+    }
+  
+    // แก้การค้นหา product
+    const product = await this.productRepository.findOneBy({ id: product_id });
+    if (!product) {
+      throw new Error('Product not found');
+    }
+  
+    const comment = this.commentRepository.create({
+      user,
+      product,
+      content,
+    });
+  
+    return this.commentRepository.save(comment);
+  }
+  
+
+  async getCommentsByProduct(product_id: number): Promise<Comment[]> {
+    return this.commentRepository.find({
+      where: { product: { id: product_id } },
+      relations: ['user', 'product'],
+      order: { createdAt: 'DESC' }
+    });
   }
 
-  // Add more methods as needed for your service
+  async getCommentsByUser(user_id: number): Promise<Comment[]> {
+    return this.commentRepository.find({
+      where: { user: { id: user_id } },
+      relations: ['user', 'product'],
+      order: { createdAt: 'DESC' }
+    });
+  }
 }
